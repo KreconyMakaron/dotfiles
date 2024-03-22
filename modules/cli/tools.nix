c565fab362921e45f4eae18b08045c1220670ba1
KreconyMakaron <krecony@tutanota.com>
2024-03-22 14:14:43 +0100

    feat: rice bat (a cat replacement)

--- modules/cli/tools.nix
+++ modules/cli/tools.nix
@@ -1,9 +1,25 @@
-{...}: {
+{pkgs, ...}: {
 	programs = {
 		eza = {
 			enable = true;
 			icons = true;
 		};
-		bat.enable = true;
+		bat = {
+			enable = true;
+			config = {
+				theme = "catppuccin-frappe";
+			};
+			themes = {
+        catppuccin-frappe = {
+          src = pkgs.fetchFromGitHub {
+            owner = "catppuccin";
+            repo = "bat";
+            rev = "2bafe4454d8db28491e9087ff3a1382c336e7d27";
+            sha256 = "yHt3oIjUnljARaihalcWSNldtaJfVDfmfiecYfbzGs0=";
+          };
+          file = "themes/Catppuccin Frappe.tmTheme";
+        };
+      };
+		};
 	};
 }
