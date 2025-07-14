a302b7d26b432ccb6624f4428179241d70f56c50
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-07-14 23:15:44 +0200

    feat: remove cringe

--- system/core/users.nix
+++ system/core/users.nix
@@ -5,7 +5,6 @@
     users = {
       krecony = {
         isNormalUser = true;
-        description = "the man the myth the legend";
         extraGroups = [
           "networkmanager"
           "wheel"
