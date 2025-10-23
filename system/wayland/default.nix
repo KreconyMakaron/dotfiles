7643d755e1aeba81d57f7d45e184075e20178376
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-10-23 10:26:17 +0200

    fix: xdg-portal didn't work because of this envar

--- system/wayland/default.nix
+++ system/wayland/default.nix
@@ -8,7 +8,6 @@
       __GL_VRR_ALLOWED = "0";
       _JAVA_AWT_WM_NONEREPARENTING = "1";
       DISABLE_QT5_COMPAT = "0";
-      GDK_BACKEND = "wayland,x11";
       ANKI_WAYLAND = "1";
       DIRENV_LOG_FORMAT = "";
       WLR_DRM_NO_ATOMIC = "1";
