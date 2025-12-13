79992fd77ea17157a28c417144ffa62109c4165b
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-12-13 22:55:36 +0100

    feat: add jack support

--- modules/system/sound.nix
+++ modules/system/sound.nix
@@ -1,14 +1,27 @@
-{...}: {
+{pkgs, user, ...}: {
   security.rtkit.enable = true;
+
   services.pipewire = {
     enable = true;
     audio.enable = true;
     wireplumber.enable = true;
     pulse.enable = true;
+    jack.enable = true;
     alsa = {
       enable = true;
       support32Bit = true;
     };
+  };
+
+  environment.systemPackages = with pkgs; [ 
+    libjack2 jack2 qjackctl pavucontrol libjack2 jack2 qjackctl jack_capture 
+  ];
+
+  users.users.${user}.extraGroups = [ "audio" ];
+
+  musnix = {
+    enable = true;
+    soundcardPciId = "00:1f.3";
   };
 
   services.pulseaudio.enable = false;
