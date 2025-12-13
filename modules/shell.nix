9147e2273a89b1136e4c2187f37b66d9bcbff94a
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-12-13 22:55:56 +0100

    feat: add nix-shell to shell prompt

--- modules/shell.nix
+++ modules/shell.nix
@@ -27,7 +27,7 @@
   programs.starship = {
     enable = true;
     settings = {
-      format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+      format = "$username$hostname$directory$git_branch$git_state$git_status$nix_shell$cmd_duration$line_break$python$character";
       palette = "style";
       palettes.style = with colors.withHashtag; {
         color_bg1 = base01;
