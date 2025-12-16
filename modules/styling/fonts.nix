4a7bc0e72d414e246844c83db65c81022f1b9892
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-12-16 21:38:48 +0100

    feat: add new pretty font

--- modules/styling/fonts.nix
+++ modules/styling/fonts.nix
@@ -1,6 +1,29 @@
 {pkgs, ...}: {
   fonts = {
-    packages = with pkgs;
+    packages = with pkgs; let
+      saira-semi-condensed = stdenv.mkDerivation {
+        pname = "saira-semi-condensed";
+        version = "1.0";
+
+        src = pkgs.fetchzip {
+          url = "https://www.omnibus-type.com/wp-content/uploads/Saira-Semi-Condensed.zip";
+          hash = "sha256-5FH9O5rINVC8/aA4xC/SsefPBwGfqzmu/+K7A89G+JU=";
+        };
+
+        installPhase = ''
+          runHook preInstall
+
+          install -m444 -Dt $out/share/fonts/truetype ttf/*.ttf
+
+          runHook postInstall
+        '';
+
+        meta = {
+          homepage = "https://www.omnibus-type.com/fonts/saira-semi-condensed/";
+          license = pkgs.lib.licenses.ofl;
+        };
+      };
+    in
       [
         jetbrains-mono
         noto-fonts
@@ -8,12 +31,13 @@
         noto-fonts-color-emoji
         hermit
         dejavu_fonts
+        saira-semi-condensed
       ]
-      ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts);
+      ++ builtins.filter lib.attrsets.isDerivation (builtins.attrValues pkgs.nerd-fonts); # all nerd-fonts
     fontconfig = {
       defaultFonts = {
         monospace = ["JetBrainsMono"];
-        sansSerif = ["Dejavu Sans"];
+        sansSerif = ["SairaSemiCondensed"];
         serif = ["Dejavu Serif"];
         emoji = ["Noto Color Emoji"];
       };
