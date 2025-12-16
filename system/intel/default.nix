859fa0daf2652f4a333b0c0f54b5a49539943dcf
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2025-12-16 20:46:58 +0100

    feat: tweak intel settings

--- system/intel/default.nix
+++ system/intel/default.nix
@@ -1,19 +1,13 @@
 {pkgs, ...}: {
-  environment.systemPackages = with pkgs; [mesa];
-
   boot = {
     kernelModules = ["i915"];
     kernelParams = ["i915.fastboot=1"];
   };
 
-  hardware.opentabletdriver.enable = true;
   hardware.graphics = {
     enable = true;
-    enable32Bit = true;
     extraPackages = with pkgs; [
       intel-media-driver
-      intel-ocl
-      intel-vaapi-driver
     ];
   };
 }
