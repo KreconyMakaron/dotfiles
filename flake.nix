1f8e9c381a07b57142cbd0639850daa713529492
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2026-02-03 22:18:27 +0100

    style: cleanup inputs

--- flake.nix
+++ flake.nix
@@ -1,19 +1,14 @@
 {
   outputs =
-    inputs@{ nixpkgs, ... }:
+    inputs@{ nixpkgs, systems, ... }:
     let
-      forAllSystems = nixpkgs.lib.genAttrs [
-        "aarch64-linux"
-        "x86_64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
+      forAllSystems = nixpkgs.lib.genAttrs (import systems);
     in
     {
       formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixfmt-tree);
 
       homeManagerModules = {
-        ags = inputs.ags.homeManagerModules.default;
+        inherit (inputs.ags.homeManagerModules) ags;
       };
 
       nixosModules = {
@@ -30,41 +25,18 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     lmms-nixpkgs.url = "github:wizardlink/nixpkgs/lmms";
     musnix.url = "github:musnix/musnix";
-    nix-mineral.url = "github:cynicsketch/nix-mineral/";
-
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
+    nix-mineral.url = "github:cynicsketch/nix-mineral";
+    systems.url = "github:nix-systems/default";
+    home-manager.url = "github:nix-community/home-manager";
     nixvim.url = "github:mikaelfangel/nixvim-config";
-
-    hyprcontrib = {
-      url = "github:hyprwm/contrib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    hyprlock = {
-      url = "github:hyprwm/hyprlock";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    stylix.url = "github:danth/stylix";
+    ags.url = "github:KreconyMakaron/ags";
+    polymc.url = "github:PolyMC/PolyMC";
 
     #https://github.com/hyprwm/Hyprland/issues/5891
     hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-
-    hyprspace = {
-      url = "github:KZDKM/Hyprspace";
-      inputs.hyprland.follows = "hyprland";
-    };
-
     xdg-portal-hyprland.url = "github:hyprwm/xdg-desktop-portal-hyprland";
-
-    # ags.url = "github:KreconyMakaron/ags";
-    # change later
-    ags.url = "path:/home/krecony/code/ags";
-
-    stylix.url = "github:danth/stylix";
-
-    polymc.url = "github:PolyMC/PolyMC";
+    hyprcontrib.url = "github:hyprwm/contrib";
+    hyprlock.url = "github:hyprwm/hyprlock";
   };
 }
