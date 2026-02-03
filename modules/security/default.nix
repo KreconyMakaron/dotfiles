60d6b90e01cbf7f5ce1232a5385492eb2b8354a2
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2026-02-03 20:35:02 +0100

    chore: remove disabling as it is already done in nix-mineral

--- modules/security/default.nix
+++ modules/security/default.nix
@@ -25,10 +25,6 @@ in {
     nix-mineral.enable = mkEnableOption "enable nix mineral";
   };
 
-  imports = builtins.map importWithStuff [
-    ./disable-unused-protocols.nix
-  ];
-
   config = {
     nix-mineral = {
       inherit (cfg.nix-mineral) enable;
