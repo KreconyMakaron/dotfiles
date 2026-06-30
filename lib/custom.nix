5b8040d521c07697a721a0c9d46c9edb64cf5f1b
krecony <55319736+krecony@users.noreply.github.com>
2026-06-30 20:30:59 +0200

    style: move custom lib options to separate file for readability
    moved the options from lib/default.nix to a separate file lib/custom.nix
    which is imported by lib/default.nix

---
+++ lib/custom.nix
@@ -0,0 +1,41 @@
+lib: {
+  mkBoolOption =
+    desc: bool:
+    lib.mkOption {
+      default = bool;
+      example = !bool;
+      description = desc;
+      type = lib.types.bool;
+    };
+
+  forAllSystems = lib.genAttrs [
+    "aarch64-darwin"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "x86_64-linux"
+  ];
+
+  getDesktopFile =
+    pkg:
+    let
+      appsDir = "${pkg}/share/applications";
+    in
+    if lib.pathExists appsDir then
+      let
+        entries = lib.readDir appsDir;
+
+        desktopFiles = lib.pipe entries [
+          lib.attrNames
+          (lib.filter (name: lib.hasSuffix ".desktop" name))
+        ];
+      in
+      if desktopFiles == [ ] then null else "${lib.last desktopFiles}"
+    else
+      null;
+
+  ifNull = new: old: if old == null then new else old;
+
+  genAttrsSame = names: value: lib.genAttrs names (_: value);
+
+  associations = import ./associations.nix;
+}
