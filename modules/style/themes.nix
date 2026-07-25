2a9604d637dc467cb1313116d494cbbc05c7692d
krecony <55319736+krecony@users.noreply.github.com>
2026-07-25 14:07:00 +0200

    feat: use the nixpkgs package instead of a github fetch

--- modules/style/themes.nix
+++ modules/style/themes.nix
@@ -4,16 +4,6 @@ let
     url = "https://raw.githubusercontent.com/Apeiros-46B/everforest-walls/refs/heads/main/nature/fog_forest_alt_1.png";
     sha256 = "sha256-IeQzvScaS107R+639JzH/Jaxo4Vp0G+wpAm3ufoYHbY=";
   };
-
-  bibata-modern-classic = pkgs.runCommand "moveUp" { } ''
-    mkdir -p $out/share/icons
-    ln -s ${
-      pkgs.fetchzip {
-        url = "https://github.com/ful1e5/Bibata_Cursor/releases/download/v2.0.6/Bibata-Modern-Classic.tar.xz";
-        hash = "sha256-jpEuovyLr9HBDsShJo1efRxd21Fxi7HIjXtPJmLQaCU=";
-      }
-    } $out/share/icons/Bibata-Modern-Classic
-  '';
 in
 {
   everforest = {
@@ -23,7 +13,7 @@ in
     cursor = {
       size = 24;
       name = "Bibata-Modern-Classic";
-      package = bibata-modern-classic;
+      package = pkgs.bibata-cursors;
     };
     autoEnable = true;
     targets = {
