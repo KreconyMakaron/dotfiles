9fc75f1b2205d8c5f9ad8a37317bcf35ae09d6a5
krecony <55319736+krecony@users.noreply.github.com>
2026-07-25 18:31:31 +0200

    feat: extract hex-to-rgb into lib

--- lib/custom.nix
+++ lib/custom.nix
@@ -18,4 +18,21 @@ lib: {
   ifNull = new: old: if old == null then new else old;
 
   genAttrsSame = names: value: lib.genAttrs names (_: value);
+
+  # turns a hex color (without hashtag) into an rgb one
+  # e.g. 123456 -> rgb(18, 52, 86)
+  hex-to-rgb =
+    with lib.strings;
+    x:
+    let
+      hex-to-dec =
+        x:
+        let
+          y = charToInt x;
+        in
+        if y >= 97 then y - 87 else y - 48;
+      merge-hex = x: y: lib.toString ((lib.elemAt x y) * 16 + (lib.elemAt x (y + 1)));
+      s = map hex-to-dec (stringToCharacters (toLower x));
+    in
+    "rgb(${merge-hex s 0}, ${merge-hex s 2}, ${merge-hex s 4})";
 }
